a50426212e2edadefaeb423face0154dbf893914
Christina L. Cole <christinalcole@gmail.com>
2015-11-11 13:06:51 -0500

    added Skillcrush soln suggestion

--- nokogiri.rb
+++ nokogiri.rb
@@ -6,10 +6,16 @@ doc = Nokogiri::HTML(open("http://www.marthastewart.com/312598/brick-pressed-san
 # search = doc.css(".components-container")
 # puts search
 
-list = doc.css(".components-item").children
-ingredients = list.to_a
+#list = doc.css(".components-item").children
+#ingredients = list.to_a
 
 puts "To make Martha's brick-pressed sandwich, you'll need:"
 ingredients.each do |i|
   puts i
 end
+
+#Skillcrush solution
+#list = doc.css ('.components-item')
+#list.each do |n|
+#  puts n.inner_html
+#end
