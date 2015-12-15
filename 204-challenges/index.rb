e21b88426006156affdbf2de07240abbbccc5e12
Christina L. Cole <christinalcole@gmail.com>
2015-12-15 15:00:56 -0500

    Changing case statement from puts to message =

--- 204-challenges/index.rb
+++ 204-challenges/index.rb
@@ -22,15 +22,15 @@ end
 
 def get_message(birth_path_num)
 case birth_path_num
-when number = 1 then puts "One is the leader.  The number one indicates the ability to stand alone, and is a strong vibration.  Ruled by the Sun."
-when number = 2 then puts "This is the mediator and peace-lover.  The number two indicates the desire for harmony.  It is a gentle, considerate, and sensitive vibration.  Ruled by the Moon."
-when number = 3 then puts "Number Three is a sociable, friendly, and outgoing vibration.  Kind, positive, and optimistic, Threes enjoy life and have a good sense of humor.  Ruled by Jupiter."
-when number = 4 then puts "This is the worker.  Practical, with a love of detail, Fours are trustworthy, hard-working, and helpful.  Ruled by Uranus."
-when number = 5 then puts "This is the freedom lover.  The number five is an intellectual vibration.  These are the 'idea' people with a love of variety and the ability to adapt to most situations.  Ruled by Mercury."
-when number = 6 then puts "This is the peace lover.  The number six is a loving, stable, and harmonious vibration.  Ruled by Venus."
-when number = 7 then puts "This is the deep thinker.  The number seven is a spiritual vibration.  These people are not very attached to material things, are introspective, and generally quiet.  Ruled by Neptune."
-when number = 8 then puts "This is the manager.  Number Eight is a strong, successful, and material vibration.  Ruled by Saturn."
-when number = 9 then puts "This is the teacher.  Number Nine is a tolerant, somewhat impractical, and sympathetic vibration.  Ruled by Mars."
+when number = 1 then message = "One is the leader.  The number one indicates the ability to stand alone, and is a strong vibration.  Ruled by the Sun."
+when number = 2 then message = "This is the mediator and peace-lover.  The number two indicates the desire for harmony.  It is a gentle, considerate, and sensitive vibration.  Ruled by the Moon."
+when number = 3 then message = "Number Three is a sociable, friendly, and outgoing vibration.  Kind, positive, and optimistic, Threes enjoy life and have a good sense of humor.  Ruled by Jupiter."
+when number = 4 then message = "This is the worker.  Practical, with a love of detail, Fours are trustworthy, hard-working, and helpful.  Ruled by Uranus."
+when number = 5 then message = "This is the freedom lover.  The number five is an intellectual vibration.  These are the 'idea' people with a love of variety and the ability to adapt to most situations.  Ruled by Mercury."
+when number = 6 then message = "This is the peace lover.  The number six is a loving, stable, and harmonious vibration.  Ruled by Venus."
+when number = 7 then message = "This is the deep thinker.  The number seven is a spiritual vibration.  These people are not very attached to material things, are introspective, and generally quiet.  Ruled by Neptune."
+when number = 8 then message = "This is the manager.  Number Eight is a strong, successful, and material vibration.  Ruled by Saturn."
+when number = 9 then message = "This is the teacher.  Number Nine is a tolerant, somewhat impractical, and sympathetic vibration.  Ruled by Mars."
 else message = "Wait!  Your birth path number isn't 1-9!!"
 end
 end
