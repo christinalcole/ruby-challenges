6feb853fb9a5d2c475bf4b9f4e04bcb5b729ac5d
Christina L. Cole <christinalcole@gmail.com>
2015-12-26 15:26:21 -0500

    Updating post-do with redirect

--- 204-challenges/numerology-app/index.rb
+++ 204-challenges/numerology-app/index.rb
@@ -40,15 +40,33 @@ else message = "Your numerology number is #{number}.  Wait!  Your birth path num
 end
 end
 
+def setup_index_view
+  birthdate = params[:birthdate]
+  number = first_num(birthdate) 
+  path_number = nn(number)
+  @message = birth_path_msg(path_number)
+end
+
+
 get '/' do
   erb :form
 end
 
 post '/' do
-  birthdate = params[:birthdate]
-  number = first_num(birthdate) 
+  number = first_num(params[:birthdate])
   path_number = nn(number)
+  redirect "/message/#{path_number}"
+  erb :index
+end
+
+get '/message/:path_number' do
+  path_number = params[:path_number].to_i
   @message = birth_path_msg(path_number)
+  erb :index
+end
+
+get '/:birthdate' do
+  setup_index_view
   erb :index
 end
 
